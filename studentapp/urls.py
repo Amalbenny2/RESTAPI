c79374dae1d122f49fe5679d07a097b7a46ce5b7
Amal benny <amalvarakil20@gmail.com>
2023-10-04 10:39:07 +0530

    Postman

--- studentapp/urls.py
+++ studentapp/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('student_list',views.student_list,name='student_list'),
+    path('student_detail/<int:pk>/',views.student_detail,name='student_detail'),
 ]
 
--- studentapp/views.py
+++ studentapp/views.py
@@ -21,3 +21,26 @@ def student_list(request):
             serializer.save()
             return Response(serializer.data,status.HTTP_201_CREATED)
         return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def student_detail(request,pk):
+    try:
+        student = Students.objects.get(pk=pk)
+    except Students.DoesNotExist:
+        return Response(status=status.HTTP_400_NOT_FOUND)
+
+
+    if request.method=='GET':
+        serializer=Studentsserializer(student)
+        return Response(serializer.data)
+
+    elif request.method=='PUT':
+        serializer= Studentsserializer(student,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method=='DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
